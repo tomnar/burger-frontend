6c0c867bc19afac70dcc50999338c173d3a274fd
Tom Quast <tomquast89@gmail.com>
2023-05-30 10:41:47 +0200

    Added styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
+
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: #BF4F74;
+`;
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Title>This is a title</Title>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
