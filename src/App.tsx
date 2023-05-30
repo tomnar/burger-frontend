dc9d46f6e6bdd75056531ca52aa62a231dd47d57
Tom Quast <tomquast89@gmail.com>
2023-05-30 11:05:55 +0200

    Added leaflet

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'leaflet/dist/leaflet.css';
 import styled from 'styled-components';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import markerIconPng from "leaflet/dist/images/marker-icon.png"
+import {Icon} from 'leaflet'
 
 const Title = styled.h1`
   font-size: 1.5em;
   text-align: center;
   color: #BF4F74;
+`;
+
+const MapWrapper = styled.div`
+  height: 800px;
+  width: 100%;
 `;
 
 function App() {
@@ -27,6 +36,20 @@ function App() {
           Learn React
         </a>
       </header>
+      <MapWrapper>
+        <MapContainer style={{height: '100%', width: '100%'}} center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[51.505, -0.09]} icon={new Icon({iconUrl: markerIconPng, iconSize: [25, 41], iconAnchor: [12, 41]})}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+      </MapWrapper>
+      
     </div>
   );
 }
